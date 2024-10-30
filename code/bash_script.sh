6ad0c1eb34ade8be3c517c88a452a57751d6cc69
Sara Tagol <sptagol@usfca.edu>
2024-10-30 10:01:08 -0700

    Added bash script

---
+++ code/bash_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+# This script renders the R Markdown file with the specified parameters
+
+# Sara Tagol
+# October 30, 2024
+# sptagol@usfca.edu
+
+
+
+# Check that exactly two arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <path_to_snp_csv> <sample_id>"
+    echo "Example: $0 data/sample_snp_data.csv SRR12345678"
+    exit 1
+fi
+
+# Assign command-line arguments to variables
+SNP_FILE="$1"
+SAMPLE_ID="$2"
+
+# Render the R Markdown file with specified parameters using Rscript
+Rscript -e "rmarkdown::render('Report.Rmd', params = list(snp_file ='$SNP_FILE', sample_id = '$SAMPLE_ID'))"
+
+chmod +x render_report.sh
+./render_report.sh data/sample_snp_data.csv SRR12345678
+
